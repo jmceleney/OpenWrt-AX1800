bde429f970c1a3e1d7f06db08a583cf1c5db822e
lvii <57lvii@gmail.com>
2022-05-12 00:42:32 +0800

    custom fork

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,13 +8,12 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+set -x
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# https://github.com/coolsnowwolf/openwrt-gl-ax1800/blob/master/feeds.conf.default
+# https://github.com/kenzok8/openwrt-packages
+# https://github.com/kenzok8/small
+echo '
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
-
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
-#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -8,24 +8,28 @@
 # Blog: https://p3terx.com
 #============================================================
 
+set -x
+
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt-AX1800/g' package/base-files/files/bin/config_generate
 
-# Modify the version number
-sed -i "s/OpenWrt /P3TERX build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# # Modify the version number
+# sed -i "s/OpenWrt /GitHub Actions Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="P3TERX"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
+    echo 'CONFIG_KERNEL_BUILD_USER="LVII"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"LVII"@' .config
 
 # Add kernel build domain
 [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
